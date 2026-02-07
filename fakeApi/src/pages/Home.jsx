eacda320931aa57aae506132b1be87e72c85fb26
Shaikh Zahid Raza <work.zahidraza@gmail.com>
2026-02-07 20:48:01 +0530

    intial fake api test

---
+++ fakeApi/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="h-screen w-screen  bg-black flex flex-col items-center justify-center">
+      <div className="h-50 w-120 bg- flex flex-col items-center justify-center gap-10 rounded-2xl">
+        <h1 className="text-5xl text-blue-500 font-extrabold whitespace-nowrap  ">
+          Explore products
+        </h1>
+        <button className="px-7 py-3 bg-blue-700 rounded-xl text-white text-xl active:scale-95">Explore</button>
+      </div>
+    </div>
+  );
+};
+export default Home;
